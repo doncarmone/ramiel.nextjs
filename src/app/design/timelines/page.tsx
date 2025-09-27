604ecd8d81a576b04260b3b9321da22b1c78a5f0
Javier Carmona <javier.carmona@wizeline.com>
2025-09-26 23:17:11 -0600

    [ADD] Progress over Components

--- src/app/design/timelines/page.tsx
+++ src/app/design/timelines/page.tsx
@@ -7,104 +7,40 @@ export default function PlaygroundPage() {
       <PageCover title='Linea del tiempo' addButtonText='Agregar Evento' />
       <div className='mt-1 px-4'>
         <ul className='timeline timeline-vertical'>
-          <li>
-            <GenericCard
-              title={ramielTimelines.data[0].event}
-              body=''
-              className='timeline-start timeline-box'
-              showActions={false}
-            />
-            <div className='timeline-middle'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='h-10 w-10'
+          {ramielTimelines.data.map((timeline, index) => (
+            <li key={timeline.id}>
+              {index > 0 && <hr />}
+              <div
+                className={
+                  timeline.sort % 2 === 0
+                    ? 'timeline-end timeline-box'
+                    : 'timeline-start timeline-box'
+                }
               >
-                <path
-                  fillRule='evenodd'
-                  d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
-                  clipRule='evenodd'
+                <GenericCard
+                  title={`Evento ${timeline.sort}`}
+                  body={timeline.event}
+                  className='w-fit max-w-sm min-h-fit'
+                  showActions={false}
                 />
-              </svg>
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className='timeline-middle'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='h-5 w-5'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </div>
-            <div className='timeline-end timeline-box'>iMac</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className='timeline-start timeline-box'>iPod</div>
-            <div className='timeline-middle'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='h-5 w-5'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className='timeline-middle'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='h-5 w-5'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </div>
-            <div className='timeline-end timeline-box'>iPhone</div>
-            <hr />
-          </li>
-          <li>
-            <hr />
-            <div className='timeline-start timeline-box'>Apple Watch</div>
-            <div className='timeline-middle'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 20 20'
-                fill='currentColor'
-                className='h-5 w-5'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </div>
-          </li>
+              </div>
+              <div className='timeline-middle'>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  viewBox='0 0 20 20'
+                  fill='currentColor'
+                  className='h-5 w-5'
+                >
+                  <path
+                    fillRule='evenodd'
+                    d='M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z'
+                    clipRule='evenodd'
+                  />
+                </svg>
+              </div>
+              {index < ramielTimelines.data.length - 1 && <hr />}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
